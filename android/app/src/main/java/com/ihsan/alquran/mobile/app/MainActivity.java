8ac2d1532fc506207205d2e3ce5af52a2c328945
Ihsan Nurul Habib <ihsan.inh@gmail.com>
2019-11-03 17:17:50 +0700

    change minor

--- android/app/src/main/java/com/ihsan/alquran/mobile/app/MainActivity.java
+++ android/app/src/main/java/com/ihsan/alquran/mobile/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.jadwalsholat;
+package com.ihsan.alquran.mobile.app;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "jadwalsholat";
+    return "AlQuranMobile";
   }
 }
--- android/app/src/main/java/com/ihsan/alquran/mobile/app/MainApplication.java
+++ android/app/src/main/java/com/ihsan/alquran/mobile/app/MainApplication.java
@@ -1,4 +1,4 @@
-package com.jadwalsholat;
+package com.ihsan.alquran.mobile.app;
 
 import android.app.Application;
 import android.content.Context;
