ce8900e067bee741e4d837456c620ab712ae2a49
Ihsan Nurul Habib <ihsan.inh@gmail.com>
2020-02-15 13:37:36 +0700

    Fix Link

--- app/Utils/EndPoints.js
+++ app/Utils/EndPoints.js
@@ -1,6 +1,6 @@
 import { BASE_URL } from 'react-native-dotenv';
 
-const quranList = `${BASE_URL}/surat`;
+const quranList = `${BASE_URL}v1/surat`;
 const quranDetail = (surahId, jmlAyat) =>
-  `${BASE_URL}/ayatweb/${surahId}/0/0/${jmlAyat}`;
+  `${BASE_URL}v1/ayatweb/${surahId}/0/0/${jmlAyat}`;
 export { quranList, quranDetail };
