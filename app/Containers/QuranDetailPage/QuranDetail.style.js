1814a90b7f63aba138ea59efccdd7f2b5077a0ed
Ihsan Nurul Habib <ihsan.inh@gmail.com>
2020-02-23 18:15:43 +0700

    Refactor styles

--- app/Containers/QuranDetailPage/QuranDetail.style.js
+++ app/Containers/QuranDetailPage/QuranDetail.style.js
@@ -4,52 +4,6 @@ import { Colors } from '../../Themes/Colors';
 import { FontType } from '../../Themes/Fonts';
 
 const Styles = StyleSheet.create({
-  CardStyle: {
-    height: 'auto',
-    margin: 10,
-  },
-  cardContainer: {
-    flex: 1,
-    flexDirection: 'row',
-  },
-  numberCircleContainer: {
-    flex: 0.8,
-  },
-  descContainer: {
-    flex: 3,
-    flexDirection: 'column',
-  },
-  NumberCircle: {
-    height: 50,
-    width: 50,
-    borderRadius: 100,
-    backgroundColor: Colors.white,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 25,
-    marginLeft: 10,
-    marginBottom: 10,
-  },
-  textNumber: {
-    color: Colors.black,
-    fontSize: 18,
-  },
-  descTextRight: {
-    textAlign: 'right',
-    paddingTop: 10,
-    paddingRight: 10,
-    fontSize: 25,
-    fontFamily: 'arabic-font',
-  },
-  descTextLeftContainer: {
-    paddingVertical: 10,
-    paddingRight: 10,
-  },
-  descTextLeft: {
-    color: Colors.grey,
-    fontSize: 14,
-    lineHeight: 25,
-  },
   headerTitle: {
     color: Colors.black,
     fontSize: 20,
--- app/Navigation/AppNavigator.style.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-const Styles = StyleSheet.create({
-  headerTitleText: {
-    marginLeft: 20,
-  },
-});
-
-export { Styles };
